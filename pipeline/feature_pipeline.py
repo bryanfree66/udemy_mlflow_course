c25a0e15c03b3a4e5064979a90a6d93fdc8ab4ab
Bryan Freeman <bryanfree66@gmail.com>
2021-07-01 11:33:20 -0500

    Modified Dockerfile to add Numpy, added imputation method to feature_pipeline.py, added passing unit test for imputation method.

--- pipeline/feature_pipeline.py
+++ pipeline/feature_pipeline.py
@@ -1,6 +1,13 @@
 import pyspark.sql.functions as F
 from pyspark.sql import DataFrame
+from pyspark.ml.feature import Imputer
 
 def load_dataset(spark, file_path) -> DataFrame:
     return spark.read.csv(file_path,inferSchema=True, header=True)
 
+def impute_missing(df, input_cols, output_cols) -> DataFrame:
+    imputer = Imputer(
+        inputCols=input_cols,
+        outputCols=output_cols
+    )
+    return imputer.setStrategy("mean").fit(df).transform(df)
