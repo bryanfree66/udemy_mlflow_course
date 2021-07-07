31ac48d6e16d466dcc8d1e16f975061cdb04eca9
Bryan Freeman <bryanfree66@gmail.com>
2021-07-07 12:43:27 -0500

    added method to utils to load dataframe, more modifications to notebook 3, set spark logging level in all notebooks.

--- utilities/spark_utils.py
+++ utilities/spark_utils.py
@@ -1,5 +1,5 @@
 import pyspark
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 
 """Get or Create a Spark SQL session and add support for MLeap."""
 def get_spark_session() -> SparkSession:
@@ -8,3 +8,7 @@ def get_spark_session() -> SparkSession:
         .config('spark.sql.execution.arrow.pyspark.enabled', 'true')
         .getOrCreate()
     )
+""" Loads and returns a Spark DataFrame from a CSV file at the specified path"""
+def load_data(spark_session, file_path) -> DataFrame:
+    return spark_session.read.csv(file_path,inferSchema=True, header=True)
+
